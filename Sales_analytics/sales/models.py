5b90e544836b54ff80093d8e7566c56da994b144
Ampalam Neelamanisai <neelamanisai2323@gmail.com>
2025-09-04 22:47:23 +0530

    models.py

--- Sales_analytics/sales/models.py
+++ Sales_analytics/sales/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from customers.models import Customer
 
-# Create your models here.
+# Create your models here
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Sale(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    sale_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.name} - {self.product.name}"
+
